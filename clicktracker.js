6f2d22426a9123420f465e61a50ee17241a0b12b
Sierra Reszczynski <77764596+sierraxrose@users.noreply.github.com>
2021-02-15 20:13:07 -0600

    Add files via upload

---
+++ clicktracker.js
@@ -0,0 +1,14 @@
+//create a variable to declare the number of clicks
+let click = 0;	
+
+
+// create a function to calculate the number of clicks
+function calculateClicks () {
+	click++;
+}
+//create a function to display how many times the button has been clicked
+
+function displayClicks () {
+	alert("You have clicked " + click + " times.")
+}
+
